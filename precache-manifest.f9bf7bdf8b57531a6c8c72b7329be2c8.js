03df86ccbc4552a8b4896816ffd891304d8e2923
mjakal <martin.jakal@telsmart.eu>
2020-08-27 11:52:22 +0200

    Updates

---
+++ precache-manifest.f9bf7bdf8b57531a6c8c72b7329be2c8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e4fbb887581e41ea3fbdaad1dadb44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33625f93e1c3de72f643",
+    "url": "./static/css/2.827caeb8.chunk.css"
+  },
+  {
+    "revision": "c8542ed6b9050d956204",
+    "url": "./static/css/main.22a21dee.chunk.css"
+  },
+  {
+    "revision": "33625f93e1c3de72f643",
+    "url": "./static/js/2.cce706dd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.cce706dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8542ed6b9050d956204",
+    "url": "./static/js/main.641add1c.chunk.js"
+  },
+  {
+    "revision": "0bc9cd22044bf3d30603f1fd91db4f62",
+    "url": "./static/js/main.641add1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babb0af17c392a4626e5",
+    "url": "./static/js/runtime-main.d14ab816.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
